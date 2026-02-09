ac296296c6025797746ad172675e88de55ed4443
Inma Gijón <igijoninf@gmail.com>
2026-02-09 16:26:21 +0100

    Búsqueda

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,5 +1,6 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
 import { HeroStats } from "@/heroes/components/HeroStats";
+import { SearchControls } from "./ui/SearchControls";
 
 export const SearchPage = () => {
   return (
@@ -8,7 +9,10 @@ export const SearchPage = () => {
         title="Búsqueda de SuperHéroes"
         description="Descubre, explora y administra super héroes"
       />
+      {/*Stats Dashboard*/}
       <HeroStats />
+      {/* Filter and search */}
+      <SearchControls/>
     </>
   );
 };
