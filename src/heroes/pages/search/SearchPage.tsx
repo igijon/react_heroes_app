61c0623b3297bcc837a5b2f1c99008d04cb0f668
Inma Gijón <igijoninf@gmail.com>
2026-02-09 16:14:01 +0100

    Estructurando componentes

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,11 +1,15 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { HeroStats } from "@/heroes/components/HeroStats";
 
 export const SearchPage = () => {
   return (
-    <CustomJumbotron
-      title="Búsqueda de SuperHéroes"
-      description="Descubre, explora y administra super héroes"
-    />
+    <>
+      <CustomJumbotron
+        title="Búsqueda de SuperHéroes"
+        description="Descubre, explora y administra super héroes"
+      />
+      <HeroStats />
+    </>
   );
 };
 
